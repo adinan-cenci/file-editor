81426242901eb965e00b9b5c8a94b501bfb3a677
Adinan Cenci <adinancenci@protonmail.com>
2026-09-09 22:16:26 +0100

    added SearchInterface

--- src/Search/Search.php
+++ src/Search/Search.php
@@ -9,7 +9,7 @@ use AdinanCenci\FileEditor\Search\Condition\OrConditionGroup;
 use AdinanCenci\FileEditor\Search\Iterator\DataIterator;
 use AdinanCenci\FileEditor\Search\Order\Order;
 
-class Search implements ConditionGroupInterface
+class Search implements SearchInterface
 {
     /**
      * @var AdinanCenci\FileEditor\File
@@ -63,11 +63,7 @@ class Search implements ConditionGroupInterface
     }
 
     /**
-     * Executes the search and returns the ordered results.
-     *
-     * @return string[]
-     *   The lines of the file that match our criteria, indexed by their
-     *   position in the file.
+     * {@inheritdoc}
      */
     public function find(): array
     {
@@ -80,10 +76,7 @@ class Search implements ConditionGroupInterface
     }
 
     /**
-     * Executes the search and returns the ordered results.
-     *
-     * @return AdinanCenci\FileEditor\Search\Iterator\DataWrapperInterface[]
-     *   An array of matching lines, each inside a metadata wrapper.
+     * {@inheritdoc}
      */
     public function retrieveAndOrder(): array
     {
@@ -102,33 +95,18 @@ class Search implements ConditionGroupInterface
     }
 
     /**
-     * Adds a new criteria to order the results by a specified property.
-     *
-     * @param array|string $property
-     *   The property to order by.
-     * @param string $direction
-     *   Ascending or descending.
-     *
-     * @return AdinanCenci\FileEditor\Search\Search
-     *   Returns itself.
+     * {@inheritdoc}
      */
-    public function orderBy(mixed $property, string $direction = 'ASC'): Search
+    public function orderBy(mixed $property, string $direction = 'ASC'): SearchInterface
     {
         $this->order->orderBy($property, $direction);
         return $this;
     }
 
     /**
-     * Adds a new criteria to order the results randomly.
-     *
-     * @param null|string $seed
-     *   If informed, the seed will be used to order the results.
-     *   The items will be order the same every time.
-     *
-     * @return AdinanCenci\FileEditor\Search\Search
-     *   Return itself.
+     * {@inheritdoc}
      */
-    public function orderRandomly(?string $seed = null): Search
+    public function orderRandomly(?string $seed = null): SearchInterface
     {
         $this->order->orderRandomly($seed);
         return $this;
@@ -177,28 +155,18 @@ class Search implements ConditionGroupInterface
     }
 
     /**
-     * Register a eager metadata getter.
-     *
-     * @param string $property
-     *   The metadata name.
-     * @param callable $callable
-     *   A closure.
+     * {@inheritdoc}
      */
-    public function setMetadataEagerGetter(string $property, mixed $callable)
+    public function setMetadataEagerGetter(string $property, mixed $callable): SearchInterface
     {
         $this->metadataEagerGetters[$property] = $callable;
         return $this;
     }
 
     /**
-     * Register a lazy metadata getter.
-     *
-     * @param string $property
-     *   The metadata name.
-     * @param callable $callable
-     *   A closure.
+     * {@inheritdoc}
      */
-    public function setMetadataLazyGetter(string $property, mixed $callable)
+    public function setMetadataLazyGetter(string $property, mixed $callable): SearchInterface
     {
         $this->metadataLazyGetters[$property] = $callable;
         return $this;
